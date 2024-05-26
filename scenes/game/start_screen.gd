dd6c767a62d6202b112954bf1c8fcf0a17ab884c
David Hu <d26hu@uwaterloo.ca>
2024-05-26 00:34:41 -0400

    move start_screen, adjust resolution logic

--- scenes/game/start_screen.gd
+++ scenes/game/start_screen.gd
@@ -5,13 +5,16 @@ var screen_size: Vector2i
 @onready var start_button: Button = $MarginContainer/VBoxContainer/VBoxContainer/StartButton
 
 var game_scene: PackedScene = preload("res://scenes/levels/level_1.tscn")
+var resolution: float = 16.0/9.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Make the window a bit bigger on startup
+	
+	# set the window size to be 70% of the height, then set the width based off the resolution
 	screen_size = DisplayServer.screen_get_size()
-	window_size = Vector2i(round(screen_size[0]*0.7),
-						   round(screen_size[0]*0.7)*window_size[1]/window_size[0])
+	window_size = Vector2i(round(screen_size.y*0.7*resolution),
+						   round(screen_size.y*0.7))
+	
 	DisplayServer.window_set_size(window_size)
 	DisplayServer.window_set_position(screen_size/2 - window_size/2)
 
